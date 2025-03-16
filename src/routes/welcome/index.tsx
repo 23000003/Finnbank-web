1a5017c8004d6497b058aa29271cdc3dda9a19a8
23000003 <kurtdicdican@gmail.com>
2025-03-16 15:44:37 +0800

    test deployment workflow

--- src/routes/welcome/index.tsx
+++ src/routes/welcome/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { showToast } from "../../utils/toast";
 
 export const Route = createFileRoute("/welcome/")({
@@ -6,18 +6,12 @@ export const Route = createFileRoute("/welcome/")({
 });
 
 function RouteComponent() {
-  const navigate = useNavigate();
-
   return (
     <div>
-      <h1>Welcome to Landing Page!!</h1>
+      <h1>Welcome to Landing Page!! SKSKSKSK</h1>
       <button onClick={() => showToast.warning("HERE")}>Show Toast</button>
-
-      {/* Button for Sign In */}
-      <button onClick={() => navigate({ to: "/welcome/signin" })}>Go to Sign In</button>
-
-      {/* Button for Sign Up */}
-      <button onClick={() => navigate({ to: "/welcome/signup" })}>Go to Sign Up</button>
+      <Link to="/welcome/signin">Go to Sign In</Link>
+      <Link to="/welcome/signup">Go to Sign Up</Link>
     </div>
   );
 }
