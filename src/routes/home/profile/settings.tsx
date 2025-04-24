17be894d2ccd8ca8365e4120336957161da6e319
ratilla-voltaire <07402931@usc.edu.ph>
2025-04-24 22:50:51 +0800

    just the tsx for the settings page

--- src/routes/home/profile/settings.tsx
+++ src/routes/home/profile/settings.tsx
@@ -83,6 +83,7 @@ function RouteComponent() {
         <button
           type="button"
           className="w-full bg-red-600 text-white py-3 px-6 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-all"
+          onClick={() => alert("Logout functionality triggered!")}
         >
           Logout
         </button>
