1b937b6d783c8bdee081476ff4774cfff0ef7706
23000003 <kurtdicdican@gmail.com>
2025-08-08 22:38:07 +0800

    feat: url config

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { API_BASE_URL } from "./url";
+
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
